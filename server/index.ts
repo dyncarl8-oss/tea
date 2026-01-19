97db83b6870af79444e81b8624fb0f5a9daab969
shankiz <princederder44@gmail.com>
2026-01-19 19:53:44 +0800

    Fix wildcard route syntax for Express 5 compatibility

--- server/index.ts
+++ server/index.ts
@@ -58,7 +58,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(publicPath));
 
     // Catch-all route for SPA
-    app.get('*', (req, res, next) => {
+    app.get('(.*)', (req, res, next) => {
         if (req.path.startsWith('/api')) {
             return next();
         }
